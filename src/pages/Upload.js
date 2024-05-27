62795f0bf86963463c8466b18491f2c61e44c5ee
s21sd <srivastavasunny359@gmail.com>
2024-05-27 19:05:31 +0530

    some

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,26 +1,73 @@
-import React from 'react'
-import './Search.css'
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import './Search.css';
+import { FaFileUpload } from 'react-icons/fa';
+import { CiImport } from 'react-icons/ci';
+import axios from 'axios'
 const Upload = () => {
+    const [file, setFile] = useState(null);
+    const [fileUrl, setFileUrl] = useState(''); // This is not an url we have to convert it to base 64
+
+    const onDrop = useCallback((acceptedFiles) => {
+        const selectedFile = acceptedFiles[0];
+        setFile(selectedFile);
+        previewImage(selectedFile);
+    }, []);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+    const previewImage = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onloadend = () => {
+            setFileUrl(reader.result);
+        };
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const result = await axios.post("http://localhost:5000/upload/uploadfiles", {
+            fileUrl: fileUrl
+        })
+        try {
+            console.log(result.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    console.log(file);
+    console.log(fileUrl);
+
     return (
-        <div>
-            <form className="file-upload-form">
-                <label for="file" className="file-upload-label">
-                    <div className="file-upload-design">
-                        <svg viewBox="0 0 640 512" height="1em">
-                            <path
-                                d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
-                            ></path>
-                        </svg>
-                        <p>Drag and Drop</p>
-                        <p>or</p>
-                        <span className="browse-button">Browse file</span>
+        <div className='flex justify-center items-center h-[500px]'>
+            <form className="file-upload-form ">
+                <label htmlFor="file" className="file-upload-label">
+                    <div className="file-upload-design" {...getRootProps()}>
+                        <input {...getInputProps()} type="file" required />
+                        {isDragActive ? (
+                            <div>
+                                <CiImport size={50} />
+                                <p>Drop the files here ...</p>
+                            </div>
+                        ) : (
+                            <div className="grid justify-center items-center gap-4">
+                                <svg className="mx-auto" viewBox="0 0 640 512" height="1em">
+                                    <path
+                                        d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
+                                    ></path>
+                                </svg>
+                                <p className="font-semibold">Drag & drop some files here, or click to select files</p>
+                            </div>
+                        )}
                     </div>
-                    <input id="file" type="file" />
                 </label>
+                <div className='mx-auto mt-4'>
+                    <button onClick={handleSubmit} className='bg-[#4ddfa7] p-3 cursor-pointer w-[100px] rounded-full text-white '>Submit</button>
+                </div>
             </form>
 
         </div>
-    )
-}
+    );
+};
 
-export default Upload
+export default Upload;
