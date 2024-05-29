5bc684381af67d0bb9162c6f4f7b9e409c19527c
s21sd <srivastavasunny359@gmail.com>
2024-05-29 14:06:02 +0530

    maked login Sign up

--- src/redux/features/auth-slice.js
+++ src/redux/features/auth-slice.js
@@ -1,23 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+
 const initialState = {
     isAuth: false,
     user: null
-}
+};
 
-export const auth = createSlice({
+const auth = createSlice({
     name: 'auth',
     initialState,
     reducers: {
         logIn: (state, action) => {
-            state.isAuth = true,
-                state.user = action.payload
+            state.isAuth = true;
+            state.user = action.payload;
         },
-        logOut: (state, action) => {
-            state.isAuth = false,
-                state.user = null
+        logOut: (state) => {
+            state.isAuth = false;
+            state.user = null;
         }
-
     }
-})
+});
+
 export const { logIn, logOut } = auth.actions;
 export default auth.reducer;
